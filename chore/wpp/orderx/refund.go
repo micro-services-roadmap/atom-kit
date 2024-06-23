0df1e3521fbe9270625eb6fcd17ea70dbfc87b01
alice52 <zzhang_xz@163.com>
2024-06-23 20:04:00 +0800

    feat(gz): add gz mw and copier with time, map

---
+++ chore/wpp/orderx/refund.go
@@ -0,0 +1,9 @@
+package orderx
+
+const (
+	RefundPROCESSING = 0 // PROCESSING
+	RefundSuccess    = 1 //"SUCCESS"
+	RefundClosed     = 2 //"CLOSED"
+	RefundAbnormal   = 3 //"ABNORMAL"
+	RefundInit       = 4 // PROCESSING
+)
