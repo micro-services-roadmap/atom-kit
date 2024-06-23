4fdd1299c38b8186ddb17290164d4f8d8df6c67a
alice52 <zzhang_xz@163.com>
2024-06-23 20:04:00 +0800

    feat(gz): add gz mw and copier with time, map

---
+++ chore/wpp/orderx/pay.go
@@ -0,0 +1,18 @@
+package orderx
+
+// 支付方式：0->未支付；1->支付宝；2->微信; 3->paypal
+const (
+	PayTypeAli    = 1
+	PayTypeWx     = 2
+	PayTypePaypal = 3
+)
+
+// 支付状态: 0->待付款；1->已付款；2->已关闭；3->退款异常；4->已退款；5->统一支付
+const (
+	TradeNotPay    = 0
+	TradeSuccess   = 1
+	TradeClosed    = 2
+	TradeRefundErr = 3
+	TradeRefundSuc = 4
+	TradeApprove   = 5
+)
